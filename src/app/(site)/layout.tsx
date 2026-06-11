77033c5d5058403b78d52a680b031a9b0ca2056f
Danish <s.danish0827@gmail.com>
2026-06-11 14:10:12 +0530

    clarity script added given by srinath

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -46,6 +46,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `}
         </Script>
         {/* --- End Google Tag Manager --- */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x58ybazzum");
+          `}
+        </Script>
       </head>
       <body>
         <noscript>
@@ -60,16 +69,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ClientModalWrapper />
         {children}
         <Footer />
-        {/* Kenyt Chatbot */}
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "x58ybazzum");
-          `}
-        </Script>
+        {/* Kenyt Chatbot */}        
         <script
           defer
           src="https://india.kenyt.ai/botapp/ChatbotUI/dist/js/bot-loader.js"
