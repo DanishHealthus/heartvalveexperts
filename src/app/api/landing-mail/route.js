52e6b51b31e79a215fadfd1ab0ad1106dcac42ac
Danish <s.danish0827@gmail.com>
2026-06-27 10:54:42 +0530

    email updated

--- src/app/api/landing-mail/route.js
+++ src/app/api/landing-mail/route.js
@@ -40,7 +40,7 @@ export async function POST(req) {
     });
     await transporter.sendMail({
       from: "ithealthus@gmail.com",
-      to: "heartvalveexperts@gmail.com, shwetha@healthus.ai, mohit@healthus.ai,info@healthus.ai ,drankurmd@gmail.com, customerservicemanager@hvshospitals.in, sachin@healthus.ai",
+      to: "heartvalveexperts@gmail.com, shwetha@healthus.ai, mohit@healthus.ai,info@healthus.ai ,drankurmd@gmail.com, customerservicemanager@hvshospitals.in, healthusads@gmail.com",
       subject: "New Consultation Request",
       html: `
         <p><b>Name:</b> ${name}</p>
